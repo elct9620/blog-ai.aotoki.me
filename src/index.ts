3f78b85cd28dda5ec1a4e5cc659423e129309eae
Aotokitsuruya <contact@aotoki.me>
2024-07-06 19:57:56 +0800

    Use itty-router AutoRouter

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,12 @@
 import { fromIttyRouter } from "chanfana";
-import { Router, cors } from "itty-router";
+import { AutoRouter, cors } from "itty-router";
 import { Suggest } from "./endpoints/suggest";
 
 const { preflight, corsify } = cors({
   origin: /aotoki.me$/,
 });
 
-const router = Router({
+const router = AutoRouter({
   before: [preflight],
   finally: [corsify],
 });
@@ -15,16 +15,5 @@ const openapi = fromIttyRouter(router, {
   docs_url: "/",
 });
 openapi.post("/v1/suggest", Suggest);
-
-// 404 for everything else
-router.all("*", () =>
-  Response.json(
-    {
-      success: false,
-      error: "Route not found",
-    },
-    { status: 404 },
-  ),
-);
 
 export default openapi;
