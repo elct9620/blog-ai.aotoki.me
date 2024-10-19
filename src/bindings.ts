76942a912681de517249cb1e07130ed2d9d8e266
Aotokitsuruya <contact@aotoki.me>
2024-10-19 20:54:03 +0800

    Add bindings from Cloudflare Workers

---
+++ src/bindings.ts
@@ -0,0 +1,7 @@
+export type Bindings = {
+  OPENAI_GATEWAY: string;
+  OPENAI_KEY: string;
+  LLM_MODEL: string;
+  TEXT_EMBEDDING_MODEL: string;
+  VECTORIZE_INDEX: string;
+};
--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,13 @@
 import { fromHono } from "chanfana";
 import { Hono } from "hono";
 import { getPath } from "hono/utils/url";
+
+import { Bindings } from "./bindings";
 import { Suggest } from "./endpoints/suggest";
 
 const BLOG_HOST = "blog.aotoki.me";
 
-const app = new Hono().basePath("/ai");
+const app = new Hono<{ Bindings: Bindings }>().basePath("/ai");
 
 const openapi = fromHono(app, {
   base: "/ai",
